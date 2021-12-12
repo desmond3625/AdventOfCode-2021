219e3bc0284597135c13c9f230e56000e09262a3
Klaudia Jusko <klaudia.jusko@student.uj.edu.pl>
2021-12-12 09:11:37 +0100

    add Day 6

---
+++ Day06/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace Day6
+{
+    class Solution
+    {
+        public ulong AddDays(int daysNumber, int[] fishAges)
+        {
+            ulong[] countByAge = new ulong[9];
+
+            for (int i = 0; i < fishAges.Length; i++)
+                countByAge[fishAges[i]]++;
+
+            for (int d = 0; d < daysNumber; d++)
+            {
+                ulong deadFish = countByAge[0];
+
+                for (int i = 0; i < countByAge.Length - 1; i++)
+                    countByAge[i] = countByAge[i + 1];
+
+                countByAge[8] = deadFish;
+                countByAge[6] += deadFish;
+            }
+
+            ulong fishCount = 0;
+            for (int i = 0; i <= 8; i++)
+                fishCount += countByAge[i];
+
+            return fishCount;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fishAges = System.IO.File
+                .ReadAllText("C:\\UJ\\AdventOfCode\\AdventOfCode\\Day6\\day6.txt")
+                .Split(",")
+                .Select(x=>Int32.Parse(x))
+                .ToArray();
+
+            Console.WriteLine(new Solution().AddDays(256,fishAges));
+        }
+    }
+}
