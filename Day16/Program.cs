5a6a65525a551ea7a1d7bb0e626d5cb697ead28f
Klaudia Jusko <klaudia.jusko@student.uj.edu.pl>
2021-12-16 19:25:24 +0100

    part 2 added

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -7,76 +7,88 @@ namespace Day16
     class Solution
     {
         private int i = 0;
-        private string binary;
-        private int versionSum;
+        private string _message;
+        private int _versionSum;
 
-        public void GetBinary(string input)
+        public void ConvertToBinary(string input)
         {
-            string binarystring = String.Join(String.Empty,
+            string binarystring = String.Join(String.Empty, 
                 input.Select(c => Convert.ToString(Convert.ToInt64(c.ToString(), 16), 2)
                     .PadLeft(4, '0')));
 
-            binary = binarystring;
+            _message = binarystring;
         }
 
-        public void DecodeValuePacket()
+        public long DecodeValuePacket()
         {
             string literalVal = "";
             bool isLast = false;
             while (!isLast)
             {
-                if (binary[i] == '0') isLast=true;
-                literalVal += binary.Substring(i + 1, 4);
+                if (_message[i] == '0') isLast = true;
+                literalVal += _message.Substring(i + 1, 4);
                 i += 5;
             }
-
-            long literalValDec = Convert.ToInt64(literalVal, 2);
-            //Console.WriteLine("Literal val: " + literalValDec);
+            return Convert.ToInt64(literalVal, 2);
         }
 
-        public void DecodeOperatorPacket()
+        public long DecodeOperatorPacket(int id)
         {
-            char lengthTypeId = binary[i];
-            i++;
+            List<long> operands = new List<long>();
+            char lengthTypeId = _message[i++];
             
             if (lengthTypeId == '0')
             {
-                int lengthOfSubpackets = Convert.ToInt32(binary.Substring(i, 15), 2);
+                int lengthOfSubpackets = Convert.ToInt32(_message.Substring(i, 15), 2);
                 i += 15;
                 int curr = i;
 
                 while (i < curr + lengthOfSubpackets)
-                    Decode();
+                    operands.Add(Decode());
             }
             else
             {
-                int numberOfSubpackets = Convert.ToInt32(binary.Substring(i, 11), 2);
+                int numberOfSubpackets = Convert.ToInt32(_message.Substring(i, 11), 2);
                 i += 11;
                 for (int k = 0; k < numberOfSubpackets; k++)
-                    Decode();
+                    operands.Add(Decode());
             }
 
+            if (id == 0) return operands.Sum();
+            if (id == 1) return operands.Aggregate(1L, (x, y) => x * y);
+            if (id == 2) return operands.Min();
+            if (id == 3) return operands.Max();
+            if (id == 5) return operands[0] > operands[1] ? 1 : 0;
+            if (id == 6) return operands[0] < operands[1] ? 1 : 0;
+
+            return operands[0] == operands[1] ? 1 : 0;
         }
 
-        public void Decode()
+        public long Decode()
         {
-            int version = Convert.ToInt32(binary.Substring(i, 3), 2);
+            int version = Convert.ToInt32(_message.Substring(i, 3), 2);
             i += 3;
-            versionSum += version;
+            _versionSum += version;
 
-            int id = Convert.ToInt32(binary.Substring(i, 3), 2);
-
+            int id = Convert.ToInt32(_message.Substring(i, 3), 2);
             i += 3;
-            
+
             if (id == 4)
-                DecodeValuePacket();
-            else
-                DecodeOperatorPacket();
+                return DecodeValuePacket();
+
+            return DecodeOperatorPacket(id);
         }
 
-        public int GetVersionSum()
+        public long Part1()
         {
-            return versionSum;
+            Decode();
+            return _versionSum;
+        }
+
+        public long Part2()
+        {
+            i = 0;
+            return Decode();
         }
     }
 
@@ -86,11 +98,11 @@ namespace Day16
         {
             string input = System.IO.File
                 .ReadAllText("C:\\AdventOfCode-2021\\AdventOfCode-2021\\Day16\\day16.txt");
-
+            
             Solution s = new Solution();
-            s.GetBinary(input);
-            s.Decode();
-            Console.WriteLine(s.GetVersionSum());
+            s.ConvertToBinary(input);
+            Console.WriteLine(s.Part1());
+            Console.WriteLine(s.Part2());
         }
     }
 }
